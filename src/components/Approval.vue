606bac07399ab60b6f29271af54cf318df26c80f
zangyunchong <yczang@jingan-china.cn>
2019-11-08 17:22:48 +0800

    update

--- src/components/Approval.vue
+++ src/components/Approval.vue
@@ -2,31 +2,40 @@
   <div>
     <x-header>审批报表</x-header>
     <div style="padding:10% 0">
-
+      <group>
+        <x-input
+          title="返回免登"
+          v-model="form.code"
+        ></x-input>
+      </group>
     </div>
   </div>
 </template>
 
 <script>
-import { XHeader, XInput, XButton } from 'vux'
-import * as dd from 'dingtalk-jsapi'
+import { XHeader, XInput, XButton, Group } from "vux";
+import * as dd from "dingtalk-jsapi";
 
 export default {
   components: {
     XHeader,
     XInput,
-    XButton
+    XButton,
+    Group
   },
   data() {
     return {
       // 钉钉企业id
-      corpId: 'dingb3df049b06530ece35c2f4657eb6378f',
+      corpId: "dingb3df049b06530ece35c2f4657eb6378f",
       // 企业自定义控件
-      spaceId: ''
-    }
+      spaceId: "",
+      form: {
+        code: ""
+      }
+    };
   },
   created() {
-    this.init()
+    this.init();
   },
   methods: {
     async init() {
@@ -34,17 +43,18 @@ export default {
       dd.runtime.permission.requestAuthCode({
         corpId: this.corpId,
         onSuccess: function(result) {
-        /*{
+          this.form.code = JSON.stringify(result);
+          /*{
             code: 'hYLK98jkf0m' //string authCode
         }*/
         },
         onFail: function(err) {
-          console.log('err')
+          console.log("err");
         }
-      })
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped>
